57e1e54e6df49397ce387255383d587c93993e4c
Felipe <antoniofelipesilva@live.com>
2023-01-09 19:11:11 -0300

    version.1.0.3

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
-import express from 'express';
+import express, { Request , Response } from 'express';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
+import mainRoutes from './routes/index';
 
 dotenv.config();
 const server = express();
@@ -12,7 +13,11 @@ server.set('views', path.join(__dirname, 'views'));
 server.engine('mustache', mustache());
 
 // Config da Paste Pubic 
-server.use(express.static(path.join(__dirname, '../public')))
+server.use(express.static(path.join(__dirname, '../public')));
 
-
+// Rotas  
+server.use(mainRoutes)
+server.use((req: Request, res: Response) => {
+  res.status(404).send('Página não encontrada!')
+})
 server.listen(process.env.PORT)
